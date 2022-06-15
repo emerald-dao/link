e62d480de72df2cd1ff5b660dc2f86080e44fc23
jacob-tucker <jacobtucker818@gmail.com>
2022-06-15 16:20:24 -0400

    add metadataviews

--- pages/index.js
+++ pages/index.js
@@ -65,11 +65,12 @@ export default function Home() {
       cadence: `
       import FLOAT from 0x2d4c3caffbeab845
       import NonFungibleToken from 0x1d7e57aa55817448
+      import MetadataViews from 0x1d7e57aa55817448
 
       pub fun main(user: Address): [String] {
         let account = getAccount(user)
         let bad: [String] = []
-        if !account.getCapability<&FLOAT.Collection{FLOAT.CollectionPublic, NonFungibleToken.CollectionPublic, NonFungibleToken.Receiver}>(FLOAT.FLOATCollectionPublicPath).check() {
+        if !account.getCapability<&FLOAT.Collection{FLOAT.CollectionPublic, NonFungibleToken.CollectionPublic, NonFungibleToken.Receiver, MetadataViews.ResolverCollection}>(FLOAT.FLOATCollectionPublicPath).check() {
           bad.append("FLOAT")
         }
 
