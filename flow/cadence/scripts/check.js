76bc889f356492732b60922835ac95bb4d84f415
Bjarte Karlsen <bjarte@bjartek.org>
2022-07-25 10:33:58 +0200

    added goats and moved to using AuthAccount in script to check if storage is present and not some link. Different solutions will link differently and we just want to see that they have to storage and not linked propper

--- flow/cadence/scripts/check.js
+++ flow/cadence/scripts/check.js
@@ -3,17 +3,22 @@ import FLOAT from 0x2d4c3caffbeab845
 import NonFungibleToken from 0x1d7e57aa55817448
 import MetadataViews from 0x1d7e57aa55817448
 import Art from 0xd796ff17107bbff6
+import GoatedGoats from 0x2068315349bdfce5
 
 pub fun main(user: Address): [String] {
-  let account = getAccount(user)
+  let account = getAuthAccount(user)
   let bad: [String] = []
-  if account.getCapability<&FLOAT.Collection{FLOAT.CollectionPublic, NonFungibleToken.CollectionPublic, NonFungibleToken.Receiver}>(FLOAT.FLOATCollectionPublicPath).check() && !account.getCapability<&FLOAT.Collection{FLOAT.CollectionPublic, NonFungibleToken.CollectionPublic, NonFungibleToken.Receiver, MetadataViews.ResolverCollection}>(FLOAT.FLOATCollectionPublicPath).check() {
+	if account.borrow<&Any>
+  if account.borrow<&NonFungibleToken.Collection>(from:FLOAT.FLOATCollectionStoragePath) && !account.getCapability<&FLOAT.Collection{FLOAT.CollectionPublic, NonFungibleToken.CollectionPublic, NonFungibleToken.Receiver, MetadataViews.ResolverCollection}>(FLOAT.FLOATCollectionPublicPath).check() {
     bad.append("FLOAT")
   }
-  if account.getCapability<&{Art.CollectionPublic}>(Art.CollectionPublicPath).check() && !account.getCapability<&Art.Collection{Art.CollectionPublic, NonFungibleToken.CollectionPublic, NonFungibleToken.Receiver, MetadataViews.ResolverCollection}>(Art.CollectionPublicPath).check() {
+  if account.borrow<&NonFungibleToken.Collection>(from:Art.CollectionStoragePath) != nil && !account.getCapability<&Art.Collection{Art.CollectionPublic, NonFungibleToken.CollectionPublic, NonFungibleToken.Receiver, MetadataViews.ResolverCollection}>(Art.CollectionPublicPath).check() {
     bad.append("Versus")
   }
 
+	if account.borrow<&NonFungibleToken.Collection>(from:GoatedGoats.CollectionStoragePath) != nil && !account.getCapability<&GoatedGoats.Collection{GoatedGoats.GoatCollectionPublic, NonFungibleToken.CollectionPublic, NonFungibleToken.Receiver, MetadataViews.ResolverCollection}>(/public/GoatCollection).check() {
+    bad.append("Goats")
+  } 
 
   return bad
 }
