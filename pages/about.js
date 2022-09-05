b59719d258e853e3975316098ea9744ea5cb74f5
lanford33 <lanfordcai@outlook.com>
2022-09-05 21:48:36 +0800

    fix: escape

--- pages/about.js
+++ pages/about.js
@@ -6,9 +6,9 @@ export default function About(props) {
       <h1 className={h1}>What is Link?</h1>
       <p className={p}>On Flow, NFTs live in your account at a storage path; this path is only accessible to you or anyone you give explicit permission during transactions.</p>
       <br></br>
-      <p className={p}>Whenever someone wants to interact with your assets (NFTs), they need to have a 'link' and a path to where the asset is stored, technically known as a capability. These capabilities can be public or private, as you will want users to be able to access certain but not all of the information or functions of your assets at this path. One example of something that is ideally publicly linked is NFT metadata so Marketplaces can display things like NFT names, and traits of the NFTs in your account. Link, the platform verifies that the proper restrictions are in place with your NFT on the public path.</p>
+      <p className={p}>{`Whenever someone wants to interact with your assets (NFTs), they need to have a 'link' and a path to where the asset is stored, technically known as a capability. These capabilities can be public or private, as you will want users to be able to access certain but not all of the information or functions of your assets at this path. One example of something that is ideally publicly linked is NFT metadata so Marketplaces can display things like NFT names, and traits of the NFTs in your account. Link, the platform verifies that the proper restrictions are in place with your NFT on the public path.`}</p>
       <br></br>
-      <p className={p}>If a collection in your wallet is showing as 'Not Correctly Linked', it simply means that when you initially interacted with this NFT, the capabilities were incorrectly added to your account and did not meet the&nbsp;
+      <p className={p}>{`If a collection in your wallet is showing as 'Not Correctly Linked', it simply means that when you initially interacted with this NFT, the capabilities were incorrectly added to your account and did not meet the `}
         <a href="https://www.flow-nft-catalog.com/"
           target="_blank"
           rel="noopener noreferrer"
